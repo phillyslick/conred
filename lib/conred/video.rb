01e3b54bd961f89f7af48b38a4f4c68d2225825a
phillyslick <noesis@philadelphiaslick.com>
2013-07-25 12:41:09 -0400

    remove sizes for responsive design

--- lib/conred/video.rb
+++ lib/conred/video.rb
@@ -4,8 +4,8 @@ require "net/http"
 module Conred
   module Video
     def initialize(arguments = {:width => 670, :height => 450, :error_message => "Video url you have provided is invalid"})
-      @width = 'auto'
-      @height = 'auto'
+      @width = arguments[:width]
+      @height = arguments[:height]
       @error_message = arguments[:error_message]
       @video_id = get_video_id_from arguments[:video_url]
     end
